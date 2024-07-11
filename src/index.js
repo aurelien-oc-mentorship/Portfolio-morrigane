49984db52f829c635bf3dcf241cae354cadd0628
Teen-raven <bakatchii@gmail.com>
2024-07-11 18:56:00 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Importez createRoot depuis "react-dom/client"
+import ReactDOM from "react-dom/client";
 import App from './App'; // Assurez-vous d'importer le composant App depuis le bon chemin
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
