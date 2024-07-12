c6ca9e46f01504a049e3e30cb277e7337f55a0d8
Teen-raven <bakatchii@gmail.com>
2024-07-12 08:46:29 +0200

    e

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
-import CV from './components/cv';
-import Footer from './components/footer';
+import CV from './components/CV';
+import Footer from './components/Footer';
 import { LanguageProvider } from './components/languagecontext';
 import './assets/app.css';
 
 function Layout({ children }) {
-  return (
-    <div className="layout">
-      {children}
-    </div>
-  );
+  return <div className="layout">{children}</div>;
 }
 
 function App() {
