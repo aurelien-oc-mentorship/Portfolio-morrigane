bd7732eba697c8623f61864dac2ea0da2af5ad67
Teen-raven <bakatchii@gmail.com>
2024-07-12 09:44:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import App from './App'; // Assurez-vous que le chemin est correct
 import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
